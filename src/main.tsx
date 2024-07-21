2b744558687ccb27033a8c0c4177106f793c55ea
SivanPesahov <sivanp27540@gmail.com>
2024-07-21 16:18:06 +0300

    front

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { UserProvider } from "./components/AuthProvider.js";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
